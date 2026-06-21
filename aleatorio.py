16011379452539cc369d4cd0865c78567d274707
isaac-developer-uwu <zipl2259@gmail.com>
2026-06-21 16:15:07 -0300

    Add files via upload

--- aleatorio.py
+++ aleatorio.py
@@ -26,6 +26,4 @@ def buscar():
 		return render_template('result.html', resultado=valor_float)
 
 if __name__ == '__main__':
-	import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
